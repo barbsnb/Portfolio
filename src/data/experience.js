0e4438860c96e14aacb4dc63ce3816c8a25da038
Barbara Banczyk <b.banczyk@gmail.com>
2025-06-26 12:31:01 +0200

    basic setup

---
+++ src/data/experience.js
@@ -0,0 +1,8 @@
+export const experience = [
+  {
+    title: "Stażystka Frontend",
+    company: "Firma X",
+    period: "Czerwiec 2024 – Sierpień 2024",
+    description: "Tworzenie komponentów React, integracja z API...",
+  },
+];
