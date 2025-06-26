119fc863ecfa0feca349e0db80bc39f99e3b4da4
Barbara Banczyk <b.banczyk@gmail.com>
2025-06-26 13:23:58 +0200

    basic data added

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 export default function About() {
   return (
     <section id="about">
-      <h2>About Me</h2>
-      <p>Jestem studentką zainteresowaną tworzeniem aplikacji webowych, szczególnie w obszarach frontend, backend i data science.</p>
+      <h2>Hi, my name is</h2>
+      <h1>Barbara Bańczyk</h1>
+      <p> 
+        I'm a final-year MSc Computer Science student with a background in robotics and a strong passion for software engineering. I've gained hands-on experience building AI projects using Python, such as creating automatic feedback tools for IT applications with OpenAI APIs and generating music with NanoGPT. I'm comfortable working with Django, React, Bootstrap, HTML, CSS, Git, REST APIs, and automated testing. I enjoy developing software that interacts with the real world and I'm always eager to learn new skills and take on new challenges.
+      </p>
+      <p>
+        For my master's thesis, I'm working on explainable AI techniques for models that classify bird sounds. The goal is to make these AI models more transparent and understandable, which helps improve trust and usability in real-world applications.
+      </p>
     </section>
   );
 }
